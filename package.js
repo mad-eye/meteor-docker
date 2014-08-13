b1e3237047eb55e85b1f82dd55fff70ca94deed1
rissem <rissem@gmail.com>
2014-08-13 13:52:21 -0800

    use newest dockerode

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     summary: "Start, stop, and monitor docker containers"
 });
 
-Npm.depends({"dockerode": "1.3.0"});
+Npm.depends({"dockerode": "2.0.1"});
 
 Package.on_use(function (api, where) {
   api.add_files(["server.js"], "server");
